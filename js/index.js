7884c29ce5b694a3cb762b3ec7ba6edc26727555
jay-kalariya <jaykalariya1209@gmail.com>
2024-06-01 13:14:13 +0530

    xzx

---
+++ js/index.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var mySwiper = new Swiper('.swiper-container', {
+      direction: 'vertical', 
+      loop: true,
+      centeredSlides: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+      },
+  
+      // Navigation arrows
+    //   navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    //   },
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+      speed: 2000,
+      effect: 'fade',
+      fadeEffect: {
+            crossFade: true,
+       },
+    });
+  });
