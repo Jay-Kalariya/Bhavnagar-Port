0e58e6e43edfb188d6012a60132d40e31f331ac4
jay-kalariya <jaykalariya1209@gmail.com>
2024-06-01 17:48:43 +0530

    dsdsd

--- js/index.js
+++ js/index.js
@@ -25,3 +25,22 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 
+  document.addEventListener('DOMContentLoaded', function() {
+    var header = document.querySelector('.header-main');
+    var lastScrollTop = 0;
+    
+    window.addEventListener('scroll', function() {
+        var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+        
+        if (currentScroll > lastScrollTop) {
+            // Scroll down
+            header.classList.add('hidden');
+        } else {
+            // Scroll up
+            header.classList.remove('hidden');
+        }
+        lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+    }, false);
+});
+
+
